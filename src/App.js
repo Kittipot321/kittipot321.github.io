a75f498ab3539faefbcb5a579ce8b14623c4bdbc
Kittipot <Kittipot321@gmail.com>
2021-06-09 13:41:44 +0700

    Fix another link page in Browser Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import Navbars from './components/Navbars';
 import Main from './pages/Main';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 import Education from './pages/Education';
 import Projects from './pages/Projects';
 function App() {
   return (
     <div>
-      <Router>
       <Navbars/>
         <Switch>
           <Route exact path="/" component={Main}/>
           <Route path="/education" component={Education}/>
           <Route path="/projects" component={Projects}/>
         </Switch>
-      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
